5cbffb98958856f7d3d43d890cf9f1bcd326bd51
Chresko08 <shubhamsrivastava08@gmail.com>
2020-09-15 23:21:38 +0530

    Testing php integration

---
+++ topPanel.php
@@ -0,0 +1,22 @@
+    <nav class="navbar navbar-expand-sm bg-white">
+        <a class="navbar-brand" href="index.html">
+    <img src="logo.png" alt="Logo" style="width:110px;">
+  </a>
+  <ul class="navbar-nav ">
+  	<li class="nav-item">
+      <a class="nav-link" href="index.html"><h4>Home</h4></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="photos.html"><h4>Photos</h4></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="login.html"><h4>Login</h4></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="about.html"><h4>About</h4></a>
+    </li>
+  </ul>
+
+</nav>
+<br>
+<br>
